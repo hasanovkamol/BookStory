eb4f1c2c535b2c3002c34a6532941bb8226bba6e
hasanovkamol <71973508+hasanovkamol@users.noreply.github.com>
2020-10-04 22:55:28 +0500

    Add project files.

---
+++ BookStory/Controller/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStory.Controllers
+{
+    public class HomeControllers:Controller
+    {
+        public string Index()
+        {
+            return "Men Contoller class dan kelyabman";
+        }
+        
+    }
+}
