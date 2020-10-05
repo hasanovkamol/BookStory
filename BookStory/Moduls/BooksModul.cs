b34073c4ad1fc913e2397ae74c98efa2bc0ba03f
hasanovkamol <71973508+hasanovkamol@users.noreply.github.com>
2020-10-05 19:25:12 +0500

    Add libry file on wwwroot Add UserStaticFile on Startup AddUserMVC on configrationServers in Startup Add Respository on Project

---
+++ BookStory/Moduls/BooksModul.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStory.Moduls
+{
+    public class BooksModul
+    {
+        public int Id { get; set; }
+        public string title { get; set; }
+        public string Author { get; set; }
+    }
+}
---
+++ BookStory/Respository/BooksRespository.cs
@@ -0,0 +1,36 @@
+﻿using BookStory.Moduls;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStory.Respository
+{
+    public class BooksRespository
+    {
+        public List<BooksModul > GetAllBooks()
+        {
+            return DataSource();
+        }
+        public BooksModul GetBookById(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+        public List<BooksModul> SearchBook(string Title,string author)
+        {
+            return DataSource().Where(x => x.title.Contains(Title) || x.Author.Contains(author)).ToList();   
+        }
+        private List<BooksModul> DataSource()
+        {
+            return new List<BooksModul>
+            {
+                new BooksModul(){Id=1,title="java",Author="HasanovKamol1"},
+                new BooksModul(){Id=2,title="c#",Author="HasanovKamol2"},
+                new BooksModul(){Id=3,title="php",Author="HasanovKamol3"},
+                new BooksModul(){Id=4,title="c++",Author="HasanovKamol4"}
+
+            };
+        }
+    }
+}
