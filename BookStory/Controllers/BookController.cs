b3cd4b54fef7fc2aca1296b36a681ccc7191dda8
hasanovkamol <71973508+hasanovkamol@users.noreply.github.com>
2020-10-07 09:52:02 +0500

    Add _ViewImport.cshtml Add _ViewLayout.cshtml Add _ViewStart.cshtml Add Static Image Add New filt on BookModuls

---
+++ BookStory/Controllers/BookController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStory.Moduls;
+using BookStory.Respository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStory.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BooksRespository _respository = null;
+        public BookController()
+        {
+            _respository = new BooksRespository();
+        }
+        public IActionResult  GetAllBook()
+        {
+            var data= _respository.GetAllBooks();
+            return View(data);
+        }
+        public BooksModul GetBook(int id)
+        {
+            return _respository.GetBookById(id);
+        }
+        public List<BooksModul> SearchBooks(string title,string Authorname)
+        {
+            return _respository.SearchBook(title,Authorname);
+        }
+    }
+}
